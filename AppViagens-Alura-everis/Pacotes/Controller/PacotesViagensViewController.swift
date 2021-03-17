7612d1a4a4e76772ef1b8932f61b9f786573d929
Jaqueline <jaquelinesantos2810@gmail.com>
2021-03-17 18:46:34 -0300

    imagens inseridas na segunda tela

---
+++ AppViagens-Alura-everis/Pacotes/Controller/PacotesViagensViewController.swift
@@ -0,0 +1,50 @@
+//
+//  PacotesViagensViewController.swift
+//  AppViagens-Alura-everis
+//
+//  Created by Jaqueline Bittencourt Santos on 17/03/21.
+//
+
+import UIKit
+
+class PacotesViagensViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate{
+    
+    @IBOutlet weak var colecaoPacotesViagens: UICollectionView!
+    let listaViagens: Array<Viagem> = ViagemDAO().retornaTodasAsViagens()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        colecaoPacotesViagens.dataSource = self
+        colecaoPacotesViagens.delegate = self
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int ) -> Int {
+        return self.listaViagens.count
+    }
+
+func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let celulaPacote = collectionView.dequeueReusableCell(withReuseIdentifier: "celulaPacote", for: indexPath) as! PacoteViagemCollectionViewCell
+    
+    let viagemAtual = listaViagens[indexPath.item]
+    
+    celulaPacote.labelTitulo.text = viagemAtual.titulo
+    celulaPacote.labelPreco.text = viagemAtual.preco
+    celulaPacote.imagemViagem.image = UIImage(named: viagemAtual.caminhoDaImagem)
+    celulaPacote.layer.borderWidth = 0.5
+    celulaPacote.layer.borderColor = UIColor(red: 85.0/255.0, green: 85.0/255.0, blue: 85.0/255.0, alpha: 85.0/255.0).cgColor
+    celulaPacote.layer.cornerRadius = 8
+    
+    celulaPacote.imagemViagem.clipsToBounds = true
+    celulaPacote.imagemViagem.layer.masksToBounds = true
+    celulaPacote.imagemViagem.contentMode = .scaleAspectFill
+    
+    return celulaPacote
+}
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionView, sizeForItemAt indexPath: IndexPath) -> CGSize{
+        let larguraCelula = collectionView.bounds.width / 2
+        return CGSize(width: larguraCelula-15, height:160)
+        }
+}
---
+++ AppViagens-Alura-everis/Pacotes/View/PacoteViagemCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  PacoteViagemCollectionViewCell.swift
+//  AppViagens-Alura-everis
+//
+//  Created by Jaqueline Bittencourt Santos on 17/03/21.
+//
+
+import UIKit
+
+class PacoteViagemCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imagemViagem: UIImageView!
+    
+    @IBOutlet weak var labelTitulo: UILabel!
+    
+    @IBOutlet weak var labelQuantidadeDeDias: UILabel!
+    @IBOutlet weak var labelPreco: UILabel!
+}
