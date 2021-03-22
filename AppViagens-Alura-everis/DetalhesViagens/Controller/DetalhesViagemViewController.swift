09a483d22bcb602c0c36c0204534a3344bd4d49b
Jaqueline <jaquelinesantos2810@gmail.com>
2021-03-21 21:52:40 -0300

    Ajustado no tamanho de iPhone 8 e implementação do scroll view

--- AppViagens-Alura-everis/DetalhesViagens/Controller/DetalhesViagemViewController.swift
+++ AppViagens-Alura-everis/DetalhesViagens/Controller/DetalhesViagemViewController.swift
@@ -6,43 +6,43 @@
 //
 
 import UIKit
+import ObjectiveC
 
 class DetalhesViagemViewController: UIViewController {
 
     @IBOutlet weak var imagemPacoteViagem: UIImageView!
-    
     @IBAction func botaoVoltar(_ sender: UIButton) {
-        self.dismiss(animated: true, completion: nil)
+        if let navigation = navigationController {
+                   navigation.popViewController(animated: true)
+               }
     }
-    
     @IBOutlet weak var labelTituloPacoteViagem: UILabel!
-    
     @IBOutlet weak var labelDescricaoPacoteViagem: UILabel!
     @IBOutlet weak var labelDataViagem: UILabel!
-    
-    
     @IBOutlet weak var labelPrecoPacoteViagem: UILabel!
-    
     @IBOutlet weak var scrollPrincipal: UIScrollView!
+    @IBAction func textFieldEntrouFoco(_ sender: UITextField) {
+        let datePickerView = UIDatePicker().date
+    }
     var pacoteSelecionado: PacoteViagem? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        NotificationCenter.default.addObserver(self, selector: #selector(aumentarScroll(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil) 
+        
         if let pacote = pacoteSelecionado {
             self.imagemPacoteViagem.image = UIImage(named: pacote.viagem.caminhoDaImagem)
             self.labelTituloPacoteViagem.text = pacote.viagem.titulo
             self.labelDataViagem.text = pacote.dataViagem
             self.labelPrecoPacoteViagem.text = pacote.viagem.preco
             self.labelDescricaoPacoteViagem.text = pacote.descricao
-           
-     
-               
-            func didReceiveMemoryWarning(){
-                super.didReceiveMemoryWarning()
-            }
-     
-       
     }
-}
+    }
+    override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+        }
+        @objc func aumentarScroll(notification: Notification){
+            self.scrollPrincipal.contentSize = CGSize(width: self.scrollPrincipal.frame.width, height: self.scrollPrincipal.frame.height + 320)
+        }
+    
 }
